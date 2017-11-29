0eccd20508bf4b3862fae97d485467e88cfea126
machado <machado@cs.ualberta.ca>
2017-11-28 22:41:29 -0700

    Code for contingency awareness.

--- contingency.cpp
+++ contingency.cpp
@@ -1,20 +1,10 @@
 /* *****************************************************************************
- * A.L.E (Arcade Learning Environment)
- * Copyright (c) 2009-2013 by Yavar Naddaf, Joel Veness, Marc G. Bellemare,
- *  Matthew Hausknecht, Marlos C. Machado and the Reinforcement Learning
- *  and Artificial Intelligence Laboratory
- * Released under the GNU General Public License; see License.txt for details. 
+ * Author: Marlos C. Machado
  *
- * Based on: Stella  --  "An Atari 2600 VCS Emulator"
- * Copyright (c) 1995-2007 by Bradford W. Mott and the Stella team
- *
- * *****************************************************************************
- *  sharedLibraryInterfaceExampleWithModes.cpp 
- *
- *  Sample code for running an agent with the shared library interface if one
- *   wants to change the game mode or game difficult (available from v.0.6.0)
+ *  Sample code for computing the number of contingent bytes in a game.
  **************************************************************************** */
 
+#include <set>
 #include <iostream>
 #include <ale_interface.hpp>
 
@@ -23,6 +13,15 @@
 #endif
 
 using namespace std;
+
+std::vector<int> subtractRAMs(ALERAM ram1, ALERAM ram2) {
+    assert(ram1.size() == ram2.size());
+    std::vector<int> result(ram1.size());
+    for(unsigned i = 0; i < ram1.size(); i++) {
+        result[i] = ram1.get(i) - ram2.get(i);
+    }
+    return result;
+}
 
 int main(int argc, char** argv) {
     if (argc < 2) {
@@ -33,50 +32,85 @@ int main(int argc, char** argv) {
     ALEInterface ale;
 
     // Get & Set the desired settings
+    ale.setInt("frame_skip", 5);
     ale.setInt("random_seed", 123);
-    //The default is already 0.25, this is just an example
-    ale.setFloat("repeat_action_probability", 0.25);
+    ale.setInt("max_num_frames_per_episode", 18000);
+    ale.setFloat("repeat_action_probability", 0.0); // we are doing planning
 
 #ifdef __USE_SDL
     ale.setBool("display_screen", true);
     ale.setBool("sound", true);
 #endif
 
-    // Load the ROM file. (Also resets the system for new settings to
-    // take effect.)
+    // Load the ROM file. (Also resets the system for new settings to take effect.)
     ale.loadROM(argv[1]);
 
-    // Get the vectors of available modes and difficulties
-    ModeVect modes = ale.getAvailableModes();
-    DifficultyVect difficulties = ale.getAvailableDifficulties();
-
-    cout << "Number of available modes: " << modes.size() << endl;
-    cout << "Number of available difficulties: " << difficulties.size() << endl;
-
     // Get the vector of legal actions
-    ActionVect legal_actions = ale.getLegalActionSet();
+    ActionVect actions = ale.getLegalActionSet();
 
-    // Play one episode in each mode and in each difficulty
-    for (int i = 0; i < modes.size(); i++){
-        for (int j = 0; j < difficulties.size(); j++){
-            
-            ale.setDifficulty(difficulties[j]);
-            ale.setMode(modes[i]);
-            // Reset after choosing difficulty and mode.
-            ale.reset_game();
-            cout << "Mode " << modes[i] << ", difficulty " << difficulties[j] << ":" << endl;
+    int reward;
+    ALERAM prev_ram, curr_ram;
+    prev_ram = ale.getRAM();
 
-            float totalReward = 0;
-            while (!ale.game_over()) {
-                Action a = legal_actions[rand() % legal_actions.size()];
-                // Apply the action and get the resulting reward
-                float reward = ale.act(a);
-                totalReward += reward;
-            }
-            cout << "Episode ended with score: " << totalReward << endl;
-            ale.reset_game();
+    std::set<int> changing_bytes;
+    std::vector<int> len_changing_bytes;
+    std::set<int> contingent_bytes;
+    std::vector<int> len_contingent_bytes;
+    std::vector<std::vector<int>> difference_rams;
+
+    while(!ale.game_over()) {
+        // Try all actions
+        for(unsigned a = 0; a < actions.size(); a++) {
+            ale.saveState();
+            reward = ale.act(actions[a]);
+            curr_ram = ale.getRAM();
+            difference_rams.push_back(subtractRAMs(prev_ram, curr_ram));
+            ale.loadState();
         }
+
+        // Test to see which bytes vary according to the action taken
+        for(unsigned i = 0; i < curr_ram.size(); i++) {
+            bool contingent = false;
+            // Pairwise combination of different actions
+            for(unsigned j = 0; j < difference_rams.size(); j++) {
+                for(unsigned k = 0; k < difference_rams.size(); k++) {
+                    if(j < k && difference_rams[j][i] - difference_rams[k][i] != 0) {
+                        contingent = true;
+                    }
+                }
+            }
+            if(contingent == true){
+                contingent_bytes.insert(i);
+            }
+        }
+
+        // Test to see which bytes vary, regardless of contingency
+        for(unsigned i = 0; i < curr_ram.size(); i++) {
+            for(unsigned j = 0; j < difference_rams.size(); j++) {
+                if(difference_rams[j][i] != 0) {
+                    changing_bytes.insert(i);
+                }
+            }
+        }
+        // Random walk
+        Action a = actions[rand() % actions.size()];
+        reward = ale.act(a);
+        prev_ram = curr_ram;
+
+        // Keeping track of the evolving length
+        len_contingent_bytes.push_back(contingent_bytes.size());
+        len_changing_bytes.push_back(changing_bytes.size());
     }
+
+    std::cout << "contingent_bytes = [";
+    for(unsigned i = 0; i < len_contingent_bytes.size(); i++) {
+        std::cout << len_contingent_bytes[i] << ", ";
+    }
+    std::cout << "]\nchanging_bytes = [";
+    for(unsigned i = 0; i < len_changing_bytes.size(); i++) {
+        std::cout << len_changing_bytes[i] << ", ";
+    }
+    std::cout << "]\n";
 
     return 0;
 }
